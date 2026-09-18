85bbb7a047257756a694d1385249984b005ceda7
anna <yakovleva.anna.g@yandex.com>
2026-09-18 23:50:16 +0700

    initial commit

--- src/main/kotlin/org/example/lessons/lesson05/homeworks/ElvisOperator.kt
+++ src/main/kotlin/org/example/lessons/lesson05/homeworks/ElvisOperator.kt
@@ -12,7 +12,7 @@ package org.example.lessons.lesson05.homeworks
 //    val baseIntensity: Double = 3.0
 //    val coefficient: Double? = 0.73 // но может быть null
 //    val baseCoefficient = 0.5
-//    val resultIntensity = 1 / baseIntensity * (coefficient ?: baseCoefficient)
+//    val resultIntensity = baseIntensity * (coefficient ?: baseCoefficient)
 //    println(resultIntensity)
 //}
 
@@ -21,19 +21,27 @@ package org.example.lessons.lesson05.homeworks
 // Контекст: Клиент оплачивает доставку груза. К стоимости доставки добавляется страховка на груз, которая составляет 0,5% от его
 // стоимости. В случае, если стоимость не указана, то берётся стандартная стоимость в $50
 // Задача: Рассчитать полную стоимость доставки.
+fun main() {
+    val defaultCost: Double = 50.0
+    val cost: Double? = 20.0 // но может быть null
+    val deliveryCost = 5.0
+    val insuranceCoefficient = 0.005
+    val insuranceCost = (cost ?: defaultCost) + insuranceCoefficient
+    val totalCost: Double = deliveryCost + insuranceCost
+    println(insuranceCost)
+    println(totalCost)
 
-val defaultCost: Double = 50.0
-val cost: Double? = 20.0 // но может быть null
-val deliveryCost = 5.0
-val insuranceCoefficient = 0.005
-
-
+}
 
 
 // Задача 3
 //
-// Контекст: Вы проводите метеорологические измерения. Одним из важных показателей является атмосферное давление, которое
-// должно быть зафиксировано. Лаборант приносит вам набор показателей, но по пути может что-нибудь потерять. Задача - сообщить
+// Контекст: Вы проводите метеорологические измерения. Одним из важных показателей является атмосферное
+// давление, которое должно быть зафиксировано. Лаборант приносит вам набор показателей, но по пути
+// может что-нибудь потерять. Задача - сообщить
 // об ошибке в случае отсутствия показаний атмосферного давления.
 
 
+val pressure: String? = "34.6" // но может быть null
+val attentionMessage = "Attention, pressure is lost"
+val pressureForLab = pressure ?: attentionMessage
